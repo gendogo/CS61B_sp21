2a1374512bf93ea3b78c906f969a015767ccc493
gendogo <whj.bnm@qq.com>
2023-10-08 21:41:41 +0800

    Finish lab06

--- lab6/capers/Dog.java
+++ lab6/capers/Dog.java
@@ -1,30 +1,42 @@
 package capers;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
+
 import static capers.Utils.*;
 
-/** Represents a dog that can be serialized.
- * @author TODO
-*/
-public class Dog { // TODO
+/**
+ * Represents a dog that can be serialized.
+ *
+ * @author whj
+ */
+public class Dog implements Serializable{
 
-    /** Folder that dogs live in. */
-    static final File DOG_FOLDER = null; // TODO (hint: look at the `join`
-                                         //      function in Utils)
+    /**
+     * Folder that dogs live in.
+     */
+    static final File DOG_FOLDER = Utils.join(CapersRepository.CAPERS_FOLDER, "dogs");
 
-    /** Age of dog. */
+    /**
+     * Age of dog.
+     */
     private int age;
-    /** Breed of dog. */
+    /**
+     * Breed of dog.
+     */
     private String breed;
-    /** Name of dog. */
+    /**
+     * Name of dog.
+     */
     private String name;
 
     /**
      * Creates a dog object with the specified parameters.
-     * @param name Name of dog
+     *
+     * @param name  Name of dog
      * @param breed Breed of dog
-     * @param age Age of dog
+     * @param age   Age of dog
      */
     public Dog(String name, String breed, int age) {
         this.age = age;
@@ -39,8 +51,13 @@ public class Dog { // TODO
      * @return Dog read from file
      */
     public static Dog fromFile(String name) {
-        // TODO (hint: look at the Utils file)
-        return null;
+        String dogFileName = name + ".txt";
+        File dogFile = join(DOG_FOLDER, dogFileName);
+        Dog dog = null;
+        if(dogFile.exists()){
+            dog = readObject(dogFile, Dog.class);
+        }
+        return dog;
     }
 
     /**
@@ -56,14 +73,23 @@ public class Dog { // TODO
      * Saves a dog to a file for future use.
      */
     public void saveDog() {
-        // TODO (hint: don't forget dog names are unique)
+        String dogFileName = name + ".txt";
+        File dogFile = join(DOG_FOLDER, dogFileName);
+        if (!dogFile.exists()){
+            try {
+                dogFile.createNewFile();
+            } catch (IOException e) {
+                throw new RuntimeException(e.getMessage());
+            }
+        }
+        Utils.writeObject(dogFile,this);
     }
 
     @Override
     public String toString() {
         return String.format(
-            "Woof! My name is %s and I am a %s! I am %d years old! Woof!",
-            name, breed, age);
+                "Woof! My name is %s and I am a %s! I am %d years old! Woof!",
+                name, breed, age);
     }
 
 }
